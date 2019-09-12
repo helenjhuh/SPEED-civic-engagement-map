34de78bd5e0145ebd62dc026c505b48d0cae7803
tony <tony@dhcp-130-58-106-235.swarthmore.edu>
2019-09-12 15:55:03 -0400

    Added some functions to the pin controller

--- src/controllers/pin.controller.js
+++ src/controllers/pin.controller.js
@@ -1,9 +1,22 @@
 const { Pin } = require("../models");
+const { SendSuccess, SendFailure, SendError } = require("../helpers/responses");
 
 exports.browse = (req, res) => {
-  Pin.find({}, (error, pins) => {});
+  Pin.find({}, (error, pins) => {
+    if (error) return SendError(res, error);
+    return SendSuccess(res, 200, { data: pins });
+  });
 };
-exports.read = (req, res) => {};
+
+exports.read = (req, res) => {
+  const { id } = req.params;
+  if (!req.params.id) return SendFailure(res, 400, "id not provided");
+  Pin.findById(id, (error, pin) => {
+    if (error) return SendError(res, error);
+    return SendSuccess(res, 200, { data: pin });
+  });
+};
+
 exports.edit = (req, res) => {};
 exports.add = (req, res) => {};
 exports.delete = (req, res) => {};
